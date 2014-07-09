b8a19e18e60d4e259ba5c6f77be31fbb947162ca
UNDEADgtr <undead_gtr@mail.ru>
2014-07-09 11:50:37 +0300

    Added reports

--- packages/maps/public/controllers/maps.js
+++ packages/maps/public/controllers/maps.js
@@ -80,4 +80,104 @@ angular.module('mean.maps').controller('MapsController', ['$scope', '$rootScope'
             $scope.info = $stateParams.markerId;
 
         }
+    ]).controller('ReportMapController', ['$scope', '$rootScope', '$stateParams', '$log',
+        function ($scope, $rootScope, $stateParams, $log) {
+
+            var reportItems = [];
+
+            reportItems.push({name: 'Activiteiten', id: '1'});
+            reportItems.push({name: 'Amsterdam.kml', id: '2'});
+            reportItems.push({name: 'Basisscholen in Nederland', id: '3'});
+            reportItems.push({name: 'Basisscholen West', id: '4'});
+            reportItems.push({name: 'Begraafplaatsen', id: '5'});
+            reportItems.push({name: 'Overzicht van Beschermde Natuurmonumenten in Nederland', id: '6'});
+            reportItems.push({name: 'BS2', id: '7'});
+            reportItems.push({name: 'Datastore Basisscholen', id: '8'});
+
+            $scope.reportItems = reportItems;
+
+            $scope.map = {
+                center: {
+                    latitude: 52.369871,
+                    longitude: 4.899551
+                },
+                zoom: 14,
+                control: {},
+                markers: [
+                    {
+                        id: 1,
+                        icon: 'packages/maps/public/assets/img/blue_marker.png',
+                        latitude: 52.374880,
+                        longitude: 4.893425,
+                        showWindow: false,
+                        title: 'Sint Nicolaasstraat 9'
+                    },
+                    {
+                        id: 2,
+                        icon: 'packages/maps/public/assets/img/blue_marker.png',
+                        latitude: 52.373366,
+                        longitude: 4.900376,
+                        showWindow: false,
+                        title: 'Zeedijk 127'
+                    },
+                    {
+                        id: 3,
+                        icon: 'packages/maps/public/assets/img/blue_marker.png',
+                        latitude: 52.371182,
+                        longitude: 4.895313,
+                        showWindow: false,
+                        title: 'Oudezijds Voorburgwal 197'
+                    }
+                ],
+                clickedMarker: {
+                    title: 'You clicked here',
+                    latitude: 52.369871,
+                    longitude: 4.899551,
+                    showWindow: null
+                },
+                events: {
+                    click: function (mapModel, eventName, originalEventArgs) {
+                        // 'this' is the directive's scope
+                        $log.log("user defined event: " + eventName, mapModel, originalEventArgs);
+
+                        var e = originalEventArgs[0];
+
+                        if (!$scope.map.clickedMarker) {
+                            $scope.map.clickedMarker = {
+                                title: 'You clicked here',
+                                latitude: e.latLng.lat(),
+                                longitude: e.latLng.lng(),
+                                showWindow: true
+                            };
+                        }
+                        else {
+                            var marker = {
+                                latitude: e.latLng.lat(),
+                                longitude: e.latLng.lng(),
+                                showWindow: true
+                            };
+                            $scope.map.clickedMarker = marker;
+                        }
+                        //scope apply required because this event handler is outside of the angular domain
+                        $scope.$apply();
+                    }
+                }
+            };
+
+            $scope.map.markers.forEach(function (marker) {
+                marker.closeClick = function () {
+                    marker.showWindow = false;
+                    $scope.$apply();
+                };
+                marker.onClicked = function () {
+                    onMarkerClicked(marker);
+                };
+            });
+
+            var onMarkerClicked = function (marker) {
+                marker.showWindow = true;
+                $scope.$apply();
+            };
+
+        }
     ]);
--- packages/maps/public/routes/maps.js
+++ packages/maps/public/routes/maps.js
@@ -8,14 +8,22 @@ angular.module('mean.maps').config(['$stateProvider',
             .state('maps', {
                 url: '/maps',
                 templateUrl: 'maps/views/maps.html'
-            })
-            .state('map by id', {
+            }).state('map by id', {
                 url: '/maps/:mapId',
                 templateUrl: 'maps/views/map.html'
             })
             .state('marker info', {
                 url: '/maps/marker/:markerId',
                 templateUrl: 'maps/views/marker-info.html'
+            }).state('report-map', {
+                url: '/report-map',
+                templateUrl: 'maps/views/report-map.html'
+            }).state('report-new', {
+                url: '/report-new',
+                templateUrl: 'maps/views/report-new.html'
+            }).state('report by id', {
+                url: '/reports/:reportId',
+                templateUrl: 'maps/views/report.html'
             });
     }
 ]);
